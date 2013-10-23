ad52840e6337f6ee5e27672745878fd963270cd8
Nick Satterly <nick.satterly@guardian.co.uk>
2013-10-23 23:35:01 +0000

    put event in message

--- riemann-send.c
+++ riemann-send.c
@@ -5,7 +5,8 @@
 int main (int argc, const char * argv[]) 
 {
 //    AMessage msg = AMESSAGE__INIT; // AMessage
-    void *buf;                     // Buffer to store serialized data
+    void *evt_buf;                     // Buffer to store serialized data
+    void *msg_buf;                     // Buffer to store serialized data
     unsigned len;                  // Length of serialized data
  /*       
     if (argc != 2 && argc != 3)
@@ -18,21 +19,30 @@ int main (int argc, const char * argv[])
      if (argc == 3) { msg.has_b = 1; msg.b = atoi(argv[2]); }
      len = amessage__get_packed_size(&msg); */
 
-    Event event = EVENT__INIT; // AMessage
-    event.state = "ok";
-    event.service = "service111";
-    event.host = "myhost";
-    event.has_metric_sint64 = 1;
-    event.metric_sint64 = 123;
+    Event evt = EVENT__INIT; // AMessage
+    evt.state = "ok";
+    evt.service = "service111";
+    evt.host = "myhost";
+    evt.has_metric_sint64 = 1;
+    evt.metric_sint64 = 123;
 
-    len = event__get_packed_size(&event);
-        
-    buf = malloc(len);
-    event__pack(&event,buf);
+    len = event__get_packed_size(&evt);
+    evt_buf = malloc(len);
+    event__pack(&evt,evt_buf);
+
+    Msg msg = MSG__INIT;
+    msg.n_events = 1;
+    msg.events = malloc(sizeof(Event) * msg.n_events);
+    msg.events[0] = &evt;
+
+    len = msg__get_packed_size(&msg);
+    msg_buf = malloc(len);
+    msg__pack(&msg,msg_buf);
         
     fprintf(stderr,"Writing %d serialized bytes\n",len); // See the length of message
-    fwrite(buf,len,1,stdout); // Write to stdout to allow direct command line piping
+    fwrite(msg_buf,len,1,stdout); // Write to stdout to allow direct command line piping
         
-    free(buf); // Free the allocated serialized buffer
+    free(msg_buf); // Free the allocated serialized buffer
+    free(evt_buf); // Free the allocated serialized buffer
     return 0;
 }
