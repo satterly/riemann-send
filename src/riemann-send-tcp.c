ce57da32d154f91e5de6e284f26604501111c50a
Nick Satterly <nick.satterly@guardian.co.uk>
2013-11-15 15:51:46 +0000

    put event in for loop

--- src/riemann-send-tcp.c
+++ src/riemann-send-tcp.c
@@ -81,97 +81,101 @@ main (int argc, const char *argv[])
   if (apr_thread_create (&thread, NULL, circuit_breaker, NULL, mp) != APR_SUCCESS)
     perror ("Failed to create thread. Exiting.\n");
 
-  Event evt = EVENT__INIT;
+  for (; !done;) {
 
-  evt.time = 1234567890;
-  evt.state = "ok";
-  evt.service = "service111";
-  evt.host = "myhost";
-  evt.description = "this is the description";
+    Event evt = EVENT__INIT;
 
-  // char *tags[] = { "one", "two", "three", NULL };
-  char raw_tags[80] = "cat,foo=bar,yes=yay!";
-  // printf ("raw tags = %s\n", raw_tags);
+    evt.time = 1234567890;
+    evt.state = "ok";
+    evt.service = "service111";
+    evt.host = "myhost";
+    evt.description = "this is the description";
 
-  int n_tags;
-  char *tags[1024] = { NULL };
+    // char *tags[] = { "one", "two", "three", NULL };
+    char raw_tags[80] = "cat,foo=bar,yes=yay!";
+    // printf ("raw tags = %s\n", raw_tags);
 
-  evt.n_tags = tokenize (raw_tags, ",", tags);
-  evt.tags = tags;
+    int n_tags;
+    char *tags[1024] = { NULL };
 
-  char raw_attrs[80] = "env=PROD,grid=MyGrid,location=paris,foo=bar";
+    evt.n_tags = tokenize (raw_tags, ",", tags);
+    evt.tags = tags;
 
-  int n_attrs;
-  char *buffer[64] = { NULL };
+    char raw_attrs[80] = "env=PROD,grid=MyGrid,location=paris,foo=bar";
 
-  n_attrs = tokenize (raw_attrs, ",", buffer);
+    int n_attrs;
+    char *buffer[64] = { NULL };
 
-  Attribute **attrs;
-  attrs = malloc (sizeof (Attribute *) * n_attrs);
+    n_attrs = tokenize (raw_attrs, ",", buffer);
 
-  int i;
-  for (i = 0; i < n_attrs; i++) {
+    Attribute **attrs;
+    attrs = malloc (sizeof (Attribute *) * n_attrs);
 
-    printf ("buffer[%d] = %s\n", i, buffer[i]);
-    char *pair[1] = { NULL };
-    tokenize (buffer[i], "=", pair);
-    printf ("attributes[%d] -> key = %s value = %s\n", i, pair[0], pair[1]);
+    int i;
+    for (i = 0; i < n_attrs; i++) {
 
-    attrs[i] = malloc (sizeof (Attribute));
-    attribute__init (attrs[i]);
-    attrs[i]->key = pair[0];
-    attrs[i]->value = pair[1];
-    free (pair[0]);
-    free (pair[1]);
-    free (buffer[i]);
+      printf ("buffer[%d] = %s\n", i, buffer[i]);
+      char *pair[1] = { NULL };
+      tokenize (buffer[i], "=", pair);
+      printf ("attributes[%d] -> key = %s value = %s\n", i, pair[0], pair[1]);
+
+      attrs[i] = malloc (sizeof (Attribute));
+      attribute__init (attrs[i]);
+      attrs[i]->key = pair[0];
+      attrs[i]->value = pair[1];
+      free (pair[0]);
+      free (pair[1]);
+      free (buffer[i]);
+    }
+    evt.attributes = attrs;
+    evt.n_attributes = n_attrs;
+    printf ("n_attrs = %d\n", n_attrs);
+
+    evt.ttl = 86400;
+
+    evt.has_metric_sint64 = 1;
+    evt.metric_sint64 = 123;
+
+    Msg riemann_msg = MSG__INIT;
+    void *buf;
+    unsigned len;
+
+    riemann_msg.n_events = 1;
+    riemann_msg.events = malloc (sizeof (Event) * riemann_msg.n_events);
+    riemann_msg.events[0] = &evt;
+
+    len = msg__get_packed_size (&riemann_msg);
+    buf = malloc (len);
+    msg__pack (&riemann_msg, buf);
+
+    for (i = 0; i < evt.n_tags; i++) {
+      printf ("tag %d %s\n", i, tags[i]);
+      free (tags[i]);
+    }
+    fprintf (stderr, "Writing %d serialized bytes\n", len);     // See the length of message
+
+    int sockfd, n;
+    struct sockaddr_in servaddr;
+
+    sockfd = socket (AF_INET, SOCK_DGRAM, 0);
+
+    bzero (&servaddr, sizeof (servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr ("127.0.0.1");
+    servaddr.sin_port = htons (5555);
+
+    sendto (sockfd, buf, len, 0, (struct sockaddr *) &servaddr, sizeof (servaddr));
+
+    for (i = 0; i < evt.n_attributes; i++) {
+      attrs[i]->key = NULL;
+      attrs[i]->value = NULL;
+      free (attrs[i]);
+    }
+    free (attrs);
+    free (riemann_msg.events);
+    free (buf);
+
   }
-  evt.attributes = attrs;
-  evt.n_attributes = n_attrs;
-  printf ("n_attrs = %d\n", n_attrs);
-
-  evt.ttl = 86400;
-
-  evt.has_metric_sint64 = 1;
-  evt.metric_sint64 = 123;
-
-  Msg riemann_msg = MSG__INIT;
-  void *buf;
-  unsigned len;
-
-  riemann_msg.n_events = 1;
-  riemann_msg.events = malloc (sizeof (Event) * riemann_msg.n_events);
-  riemann_msg.events[0] = &evt;
-
-  len = msg__get_packed_size (&riemann_msg);
-  buf = malloc (len);
-  msg__pack (&riemann_msg, buf);
-
-  for (i = 0; i < evt.n_tags; i++) {
-    printf ("tag %d %s\n", i, tags[i]);
-    free (tags[i]);
-  }
-  fprintf (stderr, "Writing %d serialized bytes\n", len);       // See the length of message
-
-  int sockfd, n;
-  struct sockaddr_in servaddr;
-
-  sockfd = socket (AF_INET, SOCK_DGRAM, 0);
-
-  bzero (&servaddr, sizeof (servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = inet_addr ("127.0.0.1");
-  servaddr.sin_port = htons (5555);
-
-  sendto (sockfd, buf, len, 0, (struct sockaddr *) &servaddr, sizeof (servaddr));
-
-  for (i = 0; i < evt.n_attributes; i++) {
-    attrs[i]->key = NULL;
-    attrs[i]->value = NULL;
-    free (attrs[i]);
-  }
-  free (attrs);
-  free (riemann_msg.events);
-  free (buf);
 
   return 0;
 }
